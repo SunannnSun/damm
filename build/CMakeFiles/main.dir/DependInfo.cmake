92260aa3ea792f318138caecaa9dc883837c40fb
Sunan Sun <sunan@seas.upenn.edu>
2023-08-08 16:31:30 -0400

    compiled version

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/sunansun/Developer/DAMM-LPVDS/DAMM/src/dpmm.cpp" "CMakeFiles/main.dir/dpmm.cpp.o" "gcc" "CMakeFiles/main.dir/dpmm.cpp.o.d"
+  "/Users/sunansun/Developer/DAMM-LPVDS/DAMM/src/dpmmDir.cpp" "CMakeFiles/main.dir/dpmmDir.cpp.o" "gcc" "CMakeFiles/main.dir/dpmmDir.cpp.o.d"
+  "/Users/sunansun/Developer/DAMM-LPVDS/DAMM/src/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
+  "/Users/sunansun/Developer/DAMM-LPVDS/DAMM/src/niw.cpp" "CMakeFiles/main.dir/niw.cpp.o" "gcc" "CMakeFiles/main.dir/niw.cpp.o.d"
+  "/Users/sunansun/Developer/DAMM-LPVDS/DAMM/src/niwDir.cpp" "CMakeFiles/main.dir/niwDir.cpp.o" "gcc" "CMakeFiles/main.dir/niwDir.cpp.o.d"
+  "/Users/sunansun/Developer/DAMM-LPVDS/DAMM/src/normal.cpp" "CMakeFiles/main.dir/normal.cpp.o" "gcc" "CMakeFiles/main.dir/normal.cpp.o.d"
+  "/Users/sunansun/Developer/DAMM-LPVDS/DAMM/src/normalDir.cpp" "CMakeFiles/main.dir/normalDir.cpp.o" "gcc" "CMakeFiles/main.dir/normalDir.cpp.o.d"
+  "/Users/sunansun/Developer/DAMM-LPVDS/DAMM/src/spectral.cpp" "CMakeFiles/main.dir/spectral.cpp.o" "gcc" "CMakeFiles/main.dir/spectral.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "/Users/sunansun/Developer/DAMM-LPVDS/DAMM/main"
+  "/Users/sunansun/Developer/DAMM-LPVDS/DAMM/main.pdb"
+  "CMakeFiles/main.dir/dpmm.cpp.o"
+  "CMakeFiles/main.dir/dpmm.cpp.o.d"
+  "CMakeFiles/main.dir/dpmmDir.cpp.o"
+  "CMakeFiles/main.dir/dpmmDir.cpp.o.d"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
+  "CMakeFiles/main.dir/niw.cpp.o"
+  "CMakeFiles/main.dir/niw.cpp.o.d"
+  "CMakeFiles/main.dir/niwDir.cpp.o"
+  "CMakeFiles/main.dir/niwDir.cpp.o.d"
+  "CMakeFiles/main.dir/normal.cpp.o"
+  "CMakeFiles/main.dir/normal.cpp.o.d"
+  "CMakeFiles/main.dir/normalDir.cpp.o"
+  "CMakeFiles/main.dir/normalDir.cpp.o.d"
+  "CMakeFiles/main.dir/spectral.cpp.o"
+  "CMakeFiles/main.dir/spectral.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
