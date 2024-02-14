256539c4f8b00646b4e2f03eb81f153ed4f6e6f0
SunannnSun <sunan@seas.upenn.edu>
2024-02-14 16:43:09 -0500

    separted the log functionality from the class and still awaiting to be done to ensure correct logging location

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse, subprocess, os, sys, json
-from damm.util import plot_tools, data_tools
+from .util import plot_tools, data_tools
 
 
 
@@ -25,7 +25,7 @@ class damm:
 
         parser.add_argument('-b', '--base' , type=int, default=0  , help='0 damm; 1 position; 2 position+directional')
         parser.add_argument('-i', '--init' , type=int, default=15 , help='Number of initial clusters, 0 is one cluster per data; default=15')
-        parser.add_argument('-t', '--iter' , type=int, default=200, help='Number of iterations')
+        parser.add_argument('-t', '--iter' , type=int, default=30, help='Number of iterations')
         parser.add_argument('-a', '--alpha', type=float, default=1, help='Concentration Factor')
 
         args = parser.parse_args()
@@ -46,7 +46,6 @@ class damm:
         self.num, self.dim = self.Data.shape  
 
 
-        print(self.num)
         # perform damm 
         command_line_args = ['time ' + os.path.join(self.file_path, "main"),
                             '--base {}'.format(self.base),
@@ -62,34 +61,12 @@ class damm:
 
         completed_process = subprocess.run(' '.join(command_line_args), input=input_data, text=True, shell=True)
         
+
         #store old data for incremental learning
         self.prev_data = data_
 
-        return completed_process.returncode
-    
 
-    def begin_next(self, next_data):
-        """
-        For incremental learning only
-        """
-
-        prev_assignment_arr = self.assignment_arr
-        next_assignment_arr = -1 * np.ones((next_data.shape[1]), dtype=np.int32)
-        comb_assignment_arr = np.concatenate((prev_assignment_arr, next_assignment_arr))
-
-        comb_data = np.hstack((self.prev_data, next_data))
-
-        self.begin(comb_data, comb_assignment_arr)
-        self.evaluate()
-        self.plot()
-
-
-
-        
-    def evaluate(self, *args_):
-        print(self.Data.shape)
-
-        # read binary output file and store assignment array
+        # extract param
         try:
             with open(os.path.join(self.log_path, "assignment.bin"), "rb") as file:
                 assignment_bin = file.read()
@@ -105,7 +82,8 @@ class damm:
         except Exception as e:
             print("Error:", e)
             sys.exit()
-        
+
+
 
         Data = self.Data
         _, _, param_dict        = data_tools.post_process(Data, assignment_arr, self.min_num)
@@ -113,21 +91,46 @@ class damm:
         reg_param_dict          = data_tools.extract_param(Data, reg_assignment_array)
         self.reg_assignment_array = reg_assignment_array
 
-        Priors = param_dict["Priors"]
-        Mu     = param_dict["Mu"]
-        Sigma  = param_dict["Sigma"]
+        self.Priors, self.Mu, self.Sigma = param_dict.values()
+
+
+        self.K = self.Priors.shape[0]
+        
+        return self.Priors, self.Mu, self.Sigma
+
+    
+
+    def begin_next(self, next_data):
+        """
+        For incremental learning only
+        """
+
+        prev_assignment_arr = self.assignment_arr
+        next_assignment_arr = -1 * np.ones((next_data.shape[1]), dtype=np.int32)
+        comb_assignment_arr = np.concatenate((prev_assignment_arr, next_assignment_arr))
+
+        comb_data = np.hstack((self.prev_data, next_data))
+
+        self.begin(comb_data, comb_assignment_arr)
+        self.plot()
+
+
+    
+    def logOut(self):
 
         json_output = {
             "name": "DAMM result",
-            "K": Priors.shape[0],
-            "M": Mu.shape[1],
-            "Priors": Priors.tolist(),
-            "Mu": Mu.ravel().tolist(),
-            "Sigma": Sigma.ravel().tolist(),
+            "K": self.K,
+            "M": self.Mu.shape[1],
+            "Priors": self.Priors.tolist(),
+            "Mu": self.Mu.ravel().tolist(),
+            "Sigma": self.Sigma.ravel().tolist(),
         }
+
         write_json(json_output, os.path.join(os.path.dirname(self.file_path), 'output.json'))
         
-        return Priors, Mu, Sigma, assignment_arr
+
+
 
     def plot(self):
 
